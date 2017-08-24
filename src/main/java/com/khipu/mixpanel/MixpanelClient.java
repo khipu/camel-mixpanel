d9b302ad6c900b14952d7ef540c02e73e58ea496
Alejandro Vera <alejandro.vera@gmail.com>
2017-08-24 14:49:01 -0300

    soporte para token y apiKey

--- src/main/java/com/khipu/mixpanel/MixpanelClient.java
+++ src/main/java/com/khipu/mixpanel/MixpanelClient.java
@@ -3,27 +3,100 @@ package com.khipu.mixpanel;
 import com.mixpanel.mixpanelapi.ClientDelivery;
 import com.mixpanel.mixpanelapi.MessageBuilder;
 import com.mixpanel.mixpanelapi.MixpanelAPI;
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.Base64;
 
 public class MixpanelClient {
-    private MessageBuilder messageBuilder;
-    public MixpanelClient(String appToken){
-        this.messageBuilder = new MessageBuilder(appToken);
-    }
-    private void verifyValidData(String data, String dataName) throws IOException {
-        if (data==null || data.trim().length()==0){
-            throw new IOException("Invalid "+dataName +" provided");
-        }
-    }
-    public void sendEvent(String distinctId, String eventName, JSONObject props) throws IOException {
-        verifyValidData(distinctId,"distinctId");
-        verifyValidData(eventName,"eventName");
-        JSONObject events = messageBuilder.event(distinctId, eventName, props);
-        ClientDelivery delivery = new ClientDelivery();
-        delivery.addMessage(events);
-        MixpanelAPI mixpanelAPI = new MixpanelAPI();
-        mixpanelAPI.deliver(delivery);
-    }
+	private static final Logger log = LoggerFactory.getLogger(MixpanelClient.class);
+	private static final int READ_TIMEOUT = 10000;
+	private static final int CONNECT_TIMEOUT = 2000;
+	private static final int BUFFER_SIZE = 256;
+	private final String token;
+	private final String apikey;
+
+	private MessageBuilder messageBuilder;
+
+	public MixpanelClient(String appToken, String apikey) {
+		this.token = appToken;
+		this.apikey = apikey;
+		this.messageBuilder = new MessageBuilder(appToken);
+	}
+
+	private void verifyValidData(String data, String dataName) throws IOException {
+		if (data == null || data.trim().length() == 0) {
+			throw new IOException("Invalid " + dataName + " provided");
+		}
+	}
+
+	public void sendEvent(String distinctId, String eventName, JSONObject props) throws IOException {
+		verifyValidData(distinctId, "distinctId");
+		verifyValidData(eventName, "eventName");
+		JSONObject events = messageBuilder.event(distinctId, eventName, props);
+		ClientDelivery delivery = new ClientDelivery();
+		delivery.addMessage(events);
+		MixpanelAPI mixpanelAPI = new MixpanelAPI();
+		mixpanelAPI.deliver(delivery);
+	}
+
+	public boolean importEvent(String disctinctId, long time, String eventName, JSONObject properties) throws JSONException, IOException {
+		properties.put("token", token);
+		properties.put("distinct_id", disctinctId);
+		properties.put("time", time);
+		JSONObject event = new JSONObject();
+		event.put("event", eventName);
+		event.put("properties", properties);
+		return sendData(event.toString(), apikey);
+	}
+
+	boolean sendData(String dataString, String apiKey) throws IOException {
+		URL endpoint = new URL("https://api.mixpanel.com/import");
+		URLConnection connection = endpoint.openConnection();
+		connection.setReadTimeout(READ_TIMEOUT);
+		connection.setConnectTimeout(CONNECT_TIMEOUT);
+		connection.setDoOutput(true);
+		connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=utf8");
+
+		byte[] utf8data = dataString.getBytes("utf-8");
+		String base64data = new String(Base64.getEncoder().encode(utf8data));
+		String encodedData = URLEncoder.encode(base64data, "utf8");
+		String encodedQuery = "data=" + encodedData + "&api_key=" + apiKey;
+		log.debug("uploading dataString {} apiKey {} encodedData {}", dataString, apiKey, encodedData);
+
+		try (OutputStream postStream = connection.getOutputStream()) {
+			postStream.write(encodedQuery.getBytes());
+		}
+
+		try (InputStream responseStream = connection.getInputStream()) {
+			String response = this.slurp(responseStream);
+			log.debug("Mixpanel import response: {}", response);
+			return response.equals("1");
+		}
+	}
+
+	private String slurp(InputStream in) throws IOException {
+		final StringBuilder out = new StringBuilder();
+		InputStreamReader reader = new InputStreamReader(in, "utf8");
+
+		char[] readBuffer = new char[BUFFER_SIZE];
+		int readCount;
+		do {
+			readCount = reader.read(readBuffer);
+			if (readCount > 0) {
+				out.append(readBuffer, 0, readCount);
+			}
+		} while (readCount != -1);
+
+		return out.toString();
+	}
 }
--- src/test/java/com/khipu/camel/MixpanelComponentTest.java
+++ src/test/java/com/khipu/camel/MixpanelComponentTest.java
@@ -13,42 +13,41 @@ import org.junit.rules.ExpectedException;
 import java.io.IOException;
 
 public class MixpanelComponentTest extends CamelTestSupport {
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
 
-    @Test
-    public void testMixpanelJsonExpected() throws Exception {
-        thrown.expect(JSONException.class);
-        thrown.expectMessage("A JSONObject text must begin with '{' at character 0");
-        Endpoint mixpanel = getMandatoryEndpoint("mixpanel:token");
-        Producer producer = mixpanel.createProducer();
-        Exchange exchange = mixpanel.createExchange();
-        exchange.getIn().setBody("");
-        producer.process(exchange);
+	@Test
+	public void testMixpanelJsonExpected() throws Exception {
+		thrown.expect(JSONException.class);
+		thrown.expectMessage("A JSONObject text must begin with '{' at character 0");
+		Endpoint mixpanel = getMandatoryEndpoint("mixpanel:token|apiKey");
+		Producer producer = mixpanel.createProducer();
+		Exchange exchange = mixpanel.createExchange();
+		exchange.getIn().setBody("");
+		producer.process(exchange);
 
-    }
+	}
 
-    @Test
-    public void testMixpanelMandatoryDistinctIdHeader() throws Exception {
-        thrown.expect(IOException.class);
-        thrown.expectMessage("Invalid distinctId provided");
-        Endpoint mixpanel = getMandatoryEndpoint("mixpanel:token");
-        Producer producer = mixpanel.createProducer();
-        Exchange exchange = mixpanel.createExchange();
-        exchange.getIn().setBody("{}");
-        producer.process(exchange);
-    }
+	@Test
+	public void testMixpanelMandatoryDistinctIdHeader() throws Exception {
+		thrown.expect(IOException.class);
+		thrown.expectMessage("Invalid distinctId provided");
+		Endpoint mixpanel = getMandatoryEndpoint("mixpanel:token|apiKey");
+		Producer producer = mixpanel.createProducer();
+		Exchange exchange = mixpanel.createExchange();
+		exchange.getIn().setBody("{}");
+		producer.process(exchange);
+	}
 
-    @Test
-    public void testMixpanelMandatoryEventNameHeader() throws Exception {
-        thrown.expect(IOException.class);
-        thrown.expectMessage("Invalid eventName provided");
-        Endpoint mixpanel = getMandatoryEndpoint("mixpanel:token");
-        Producer producer = mixpanel.createProducer();
-        Exchange exchange = mixpanel.createExchange();
-        exchange.getIn().setBody("{}");
-        exchange.getIn().setHeader(MixpanelHeaders.MIXPANEL_DISCTINCT_ID,"1234");
-        producer.process(exchange);
-    }
-
+	@Test
+	public void testMixpanelMandatoryEventNameHeader() throws Exception {
+		thrown.expect(IOException.class);
+		thrown.expectMessage("Invalid eventName provided");
+		Endpoint mixpanel = getMandatoryEndpoint("mixpanel:token|apiKey");
+		Producer producer = mixpanel.createProducer();
+		Exchange exchange = mixpanel.createExchange();
+		exchange.getIn().setBody("{}");
+		exchange.getIn().setHeader(MixpanelHeaders.MIXPANEL_DISCTINCT_ID, "1234");
+		producer.process(exchange);
+	}
 }
